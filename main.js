e02cbff2a3c336e13725249eb26dd532cc8a7e2f
Osman Zakir <osmanzakir90@hotmail.com>
2021-08-14 00:32:57 +0500

    configured build environment and set up Electron, & tried to setup notification button (but it's not working)

---
+++ main.js
@@ -0,0 +1,45 @@
+// Main Process (responsible for running main file (in this case main.js))
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = !app.isPackaged;
+
+const createWindow = () => {
+  // BrowserWindow <- Renderer Process
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  win.loadFile("index.html");
+  isDev && win.webContents.openDevTools();
+};
+
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron")
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  // if not running on Mac
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// if running on Mac
+if (process.platform === "darwin") {
+  app.on("activate", () => {
+    // if all window are closed
+    // (getAllWindows array is empty)
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+}
