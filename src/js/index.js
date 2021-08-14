bbd6489d2039e0b99b88fb3e0ebf3495d1df0f1f
Osman Zakir <osmanzakir90@hotmail.com>
2021-08-14 21:29:24 +0500

    changed main div id from electronChat to chatApp to match CSS selector

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,4 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("electronApp"));
+ReactDOM.render(<App />, document.getElementById("chatApp"));
